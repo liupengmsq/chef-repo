97745588d89a344265acf46c73cc5c411b984fc5
Peng Liu <pliu@lattice-engines.com>
2022-08-06 21:23:26 +0800

    test

--- cookbooks/example/recipes/default.rb
+++ cookbooks/example/recipes/default.rb
@@ -1,8 +1,16 @@
-# This is a Chef Infra Client recipe file. It can be used to specify resources
-# which will apply configuration to a server.
+node_instance = search(:node, "hostname:#{node['hostname'].upcase}").first
+Chef::Log.info "This is an instance object as returned by Chef Server.EC2 Instance ID is #{node_instance}"
 
-log "Welcome to Chef Infra Client, #{node['example']['name']}!" do
-  level :info
+aws_instance = search(:aws_opsworks_instance, "hostname:#{node['hostname'].upcase}").first
+Chef::Log.info "This is the AWS OpsWorks instance object. It has AWS OpsWorks specific attributes, like the Layer IDs for the instance: #{aws_instance['layer_ids'].join(',')}"
+
+layer = ""
+aws_instance['layer_ids'].each do |layer_id|
+  layer = search(:aws_opsworks_layer, "layer_id:#{layer_id}").first
+  Chef::Log.info "The instance belongs to layer #{layer['layer_id']}, it's name is #{layer['name']}"
+  node.default['layer_name']=layer['name']
 end
 
-# For more information, see the documentation: https://docs.chef.io/recipes.html
+yum_package "unzip" do
+  action :install
+end
